88276b57f3e8dbb986b9023b3f0bab346858b69d
nbandari1 <133066486+nbandari1@users.noreply.github.com>
2024-02-27 21:26:36 -0500

    Update server.py

--- q3/server.py
+++ q3/server.py
@@ -3,14 +3,17 @@ import threading
 import pickle
 
 def handle_client(client_socket, client_address):
+    """ Function handles client connections """
     try:
         while True:
+            #receives pickled messages from client
             pickled_message = client_socket.recv(4096)
+            #check if message is empty or not
             if not pickled_message:
                 break
-              
+            #unpickling message  
             message = pickle.loads(pickled_message)
-
+            #send message to all clients except the sender client
             for c in clients:
                 if c != client_socket:
                     c.sendall(pickled_message)
@@ -20,16 +23,21 @@ def handle_client(client_socket, client_address):
         client_socket.close()
 
 def main():
+    #create socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #bind socket
     server_socket.bind(('localhost', 12345))
+    #listen for incoming connections
     server_socket.listen(5)
     print("Server is listening for incoming connections...")
 
     while True:
+        #accept incoming connection
         client_socket, client_address = server_socket.accept()
         print(f"Connected to client: {client_address}")
+        #add the client socket to the list of connected clients
         clients.append(client_socket)
-
+        #creating a thread to handle the client connection
         client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
         client_thread.start()
 
