903b1f03956cddc8283a2ad18403a0a6801f03bb
nbandari1 <133066486+nbandari1@users.noreply.github.com>
2024-02-27 21:49:14 -0500

    Update client.py

--- q2/client.py
+++ q2/client.py
@@ -1,6 +1,6 @@
 import socket
 import pickle
-from task_functions import perform_multiplication_task
+from task_functions import perform_division_task
 
 def distribute_task(task, arg, worker_address):
     """ Distribute a task to multiple worker nodes and collect results """
@@ -33,7 +33,7 @@ def distribute_task(task, arg, worker_address):
 def main():
     worker_address = [('127.0.0.1', 12346), ('127.0.0.1', 12347)] 
     task_arguments = (5, 10)
-    results = distribute_task(perform_multiplication_task, task_arg, worker_addresses)
+    results = distribute_task(perform_division_task, task_arg, worker_addresses)
     print("Results:", results)
 
 if __name__ == "__main__":
