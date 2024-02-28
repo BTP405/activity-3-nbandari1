645b4df78d60519f0c235befdf631e96db5f32cc
nbandari1 <133066486+nbandari1@users.noreply.github.com>
2024-02-27 21:42:25 -0500

    Update server.py

--- q1/server.py
+++ q1/server.py
@@ -1,15 +1,19 @@
 import os
 import socket
 
-def recv_file(data, dir, filename):
+def save_file(data, dir, filename):
   """ Saves the received file data to a specified directory with the provided filename """
+  #create directory if it doesn't exist
   os.makedirs(dir, exist_ok = True)
+  #create the file path
   f_path = os.path.join(dir, filename)
+  #write the received data to the file
   with open(f_path, 'wb') as f:
     f.write(data)
 
 def main():
 """ Main function which starts the server and handles incoming connections """
+#define the server IP address, port, buffer size for data transmission and directory for the received files
 host = '127.0.0.1'
 port = 12345
 buffer_size = 4096
@@ -29,12 +33,16 @@ while True:
     if not data:
       print("No data received.")
       continue
+      
+    #get file name from user input
     filename = input("Enter the filename: ")
+    #save received data to file with the entered file name
     save_file(data, dir, filename)
     print("File saved successfully as", filename)
   except Exception as err:
     print("Error: ", err)
   finally:
+    #close socket
     client_socket.close()
 
 if __name__ == "__main__":
