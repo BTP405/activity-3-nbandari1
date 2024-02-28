38a7ce6a074a647a2002ca15813631a6b2785fac
nbandari1 <133066486+nbandari1@users.noreply.github.com>
2024-02-27 21:35:13 -0500

    Update server.py

--- q2/server.py
+++ q2/server.py
@@ -5,7 +5,9 @@ from task_functions import task_multiply
 def execute_task(task_data):
     """ Execute the task received from the client """
     try:
+        #unpickle the task data to get the task function and arguments
         task, args = pickle.loads(task_data)
+        #execute task function with provided arguments
         result = task(*args)
         return result
     except Exception as e:
@@ -15,12 +17,16 @@ def execute_task(task_data):
 def main():
     HOST = '127.0.0.1'
     PORT = 12346
+    #create a socket object
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        #bind the socket
         server_socket.bind((HOST, PORT))
+        #listen for incoming connections
         server_socket.listen()
         print("Worker node is listening for incoming connections...")
 
         while True:
+            #accept a new connection
             conn, addr = server_socket.accept()
             print(f"Connection established from {addr}")
             with conn:
@@ -28,8 +34,9 @@ def main():
                 if not task_data:
                     print("No task data received")
                     continue
-                  
+                #execute the task and get the result  
                 result = execute_task(task_data)
+                #if result is not None, send it back to the client
                 if result is not None:
                     result_data = pickle.dumps(result)
                     conn.sendall(result_data)
